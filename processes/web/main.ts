664d136928c389fbe550c7ec135f2badcbc9d73c
Tamás Batka <tamas.batka@pepperlabs.io>
2022-11-14 22:39:56 +0100

    Allow cors

--- processes/web/main.ts
+++ processes/web/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../../src/AppModule';
+import { NotFoundException } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['verbose'],
+    cors: {
+      origin: (origin, cb) => {
+        if (origin && !JSON.parse(process.env.CORS_ORIGINS || '[]').includes(origin)) {
+          return cb(new NotFoundException());
+        }
+        cb(null, true);
+      },
+      credentials: true
+    }
+  });
+  await app.listen(3001);
 }
 bootstrap();
